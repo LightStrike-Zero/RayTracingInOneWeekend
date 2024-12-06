83084e921d028204925193eb53b3517443693dff
Shaunzy <68426146+LightStrike-Zero@users.noreply.github.com>
2024-12-06 20:44:53 +0800

    Added a little menu when running to select some of the ray generation options, along with a readme and build instructions.

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-This was my first attempt at following the Ray Tracing in One Weekend books. 
+This was my first attempt at following the Ray Tracing in One Weekend books, I only completed the first book.
+See here for the series https://raytracing.github.io/
 
-I plan to revist this and use shaders instead of CPU
+I plan to revisit this and use shaders instead of the CPU
+
+### Building
+This project was designed using cmake in CLion so you will need to have cmake installed to build this project
+run cmake with `cmake --build`
+run the executable that is created
+
+### Running
+It will prompt you for the image width in pixels
+then the samples (rays to shoot per pixel)
+then the bounces each pixel takes
+
+The higher these numbers the better the image but the longer it will take to render
+This is currently CPU bound so it will take a while to render, an image
+of 1000 pixels wide and 50 samples and 20 bounces will take about an hour to render
--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/*
+ *  Welcome to my ray tracing in one weekend project!
+ *  https://raytracing.github.io/books/RayTracingInOneWeekend.html
+ *
+ *  By Shaun Matthews
+ *
+ *  Last updated 6/12/2024
+ */
+
 #include "utility.h"
 
 #include "camera.h"
@@ -8,40 +17,6 @@
 
 #include <chrono>
 
-// int main() {
-//     hittable_list world;
-//
-//     auto material_ground = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-//     auto material_center = make_shared<lambertian>(color(0.0, 0.34, 0.71));
-//     auto material_left   = make_shared<dielectric>(1.50);
-//     auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
-//     auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.2);
-//
-//     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-//     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
-//     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-//     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
-//     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
-//
-//     camera cam;
-//
-//     cam.aspect_ratio = 16.0 / 9.0;
-//     cam.image_width  = 400;
-//     cam.samples_per_pixel = 300;
-//     cam.max_depth = 50;
-//
-//     cam.vfov = 25;
-//     cam.lookfrom = point3(-2,2,1);
-//     cam.lookat   = point3(0,0,-1);
-//     cam.vup      = vec3(0,1,0);
-//
-//     cam.defocus_angle = 10.0;
-//     cam.focus_dist    = 3.4;
-//
-//     cam.render(world);
-//
-//     return 0;
-// }
 
 int main() {
     hittable_list world;
@@ -89,10 +64,20 @@ int main() {
 
     camera cam;
 
+    int image_width, samples_per_pixel, max_depth;
+    std::cout << "Enter image width: ";
+    std::cin >> image_width;
+
+    std::cout << "Enter samples per pixel: ";
+    std::cin >> samples_per_pixel;
+
+    std::cout << "Enter max depth: ";
+    std::cin >> max_depth;
+
     cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 1200;
-    cam.samples_per_pixel = 500;
-    cam.max_depth         = 50;
+    cam.image_width       = image_width;
+    cam.samples_per_pixel = samples_per_pixel;
+    cam.max_depth         = max_depth;
 
     cam.vfov     = 20;
     cam.lookfrom = point3(13,2,3);
